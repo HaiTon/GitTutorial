ac14d75356d1b793c4aa8e4324dfb02acdcfe7b3
Hai <tnk7200@gmail.com>
2016-01-25 08:26:52 -0800

    add sha1 hash powershell script.

---
+++ GetSha1HashOfAFilePowershellScript.ps1
@@ -0,0 +1,5 @@
+﻿
+$file = 'C:\Users\hait\Desktop\GitTraining\FileForSHA1Demo\audacity-win-2.1.2.exe'
+
+$sha1 = New-Object System.Security.Cryptography.SHA1CryptoServiceProvider 
+[System.BitConverter]::ToString( $sha1.ComputeHash([System.IO.File]::ReadAllBytes($file)))
